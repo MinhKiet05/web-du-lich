407443ed4a8c588dee6b898b7ff5fffe629b2bb1
unknown <baog0299@gmail.com>
2026-01-01 22:26:36 +0700

    Đổi màu background

--- src/pages/aboutPage/AboutPage.tsx
+++ src/pages/aboutPage/AboutPage.tsx
@@ -5,7 +5,7 @@ export default function AboutPage() {
   return (
     <div className={styles.aboutPage}>
       <div className={styles.container}>
-        <h1>Về chúng tôi</h1>
+        <h1>Về chúng tôi, anh Kiệt béo</h1>
         <p>Chào mừng bạn đến với công ty du lịch của chúng tôi!</p>
         <div className={styles.content}>
           <p>Chúng tôi chuyên cung cấp các dịch vụ du lịch chất lượng cao...</p>
